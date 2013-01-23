5a57a10e9f357bf38d573adbfbc9ecde6b42353c
Cameron Bytheway <cameron@nujii.com>
2013-01-23 14:34:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+simple-proxy
+============
+
+Simple proxy middleware for connect/express
+
+Usage
+-----
+
+Create an http app
+```js
+var express = require("express")
+  , proxy = require("simple-proxy");
+
+
+var app = express();
+
+app.use(proxy("/api", "my.other.host.com"));
+```
+
+Make the request
+```sh
+$ curl http://localhost:5000/api
+
+<h1>Welcome to my.other.host.com</h1>
+```
+
+Options
+-------
+
+proxy(route, host, port, https)
+
+### route (string i.e. "/api")
+
+Route on which to mount proxy
+
+### host (string i.e. "google.com")
+
+Target host
+
+### port (integer)
+
+Target port
+
+### https (bool)
+
+Target uses https
---
+++ index.js
@@ -0,0 +1,31 @@
+
+/**
+ * Module dependencies
+ */
+var httpProxy = require("http-proxy"),
+    proxy = new httpProxy.RoutingProxy();
+
+module.exports = function(path, host, port, https) {
+  if (!port) port = 80;
+  if (!https) https = true;
+
+  return function(req, res, next) {
+    // Are we connecting to the proxy?
+    if (req.url.indexOf(path) === 0) {
+   
+      // Remove the the path from the url
+      req.url = req.url.replace(path, "");
+   
+      // replace the host or your network will freak out
+      req.headers.host = host;
+   
+      // proxy the bad boy
+      return proxy.proxyRequest(req, res, {
+        port: port,
+        host: host,
+        https: https
+      });
+    }
+    next();
+  }
+}
