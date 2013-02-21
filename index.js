de336fc3a9604edd02fe505156ff3341d525bd24
Cameron Bytheway <cameron@nujii.com>
2013-02-21 16:22:41 -0700

    Use the built-in http client

--- index.js
+++ index.js
@@ -2,41 +2,86 @@
 /**
  * Module dependencies
  */
-var superagent = require("superagent")
-  , http = require('http');
+var url = require('url')
+  , join = require('path').join
+  , debug = require('debug')("simple-http-proxy")
+  , protocols = {
+      http: require('http'),
+      https: require('https')
+    };
 
-// Don't let superagent serialize anything
-superagent.serialize = {};
+module.exports = function(endpoint, opts) {
+  if(!opts) opts = {};
 
-module.exports = function(endpoint) {
+  var parsedUrl = url.parse(endpoint);
+
   return function simpleHttpProxy(req, res, next) {
     // Remove the host header
-    var hostInfo = req.headers.host.split(":")
-      , host = hostInfo[0]
-      , port = hostInfo[1]
-      , resPath = req.originalUrl.replace(req.url, "");
     delete req.headers.host;
 
     // Optionally delete cookie
-    delete req.headers.cookie;
+    if(opts.cookies === false) delete req.headers.cookie;
 
-    // We'll need to add a / if it's not on there
-    if(resPath.indexOf("/") === -1) resPath = "/"+resPath;
+    // Should we keep the trailing slash?
+    var trailingSlash = req.originalUrl[req.originalUrl.length-1] === "/";
 
-    // Send it through superagent
-    var request = superagent(req.method, endpoint+req.url)
-      .buffer(false)
-      .set(req.headers)
-      .set({
-        "x-forwarded-host": host,
-        "x-forwarded-proto": req.connection.encrypted ? "https" : "http",
-        "x-forwarded-path": resPath
-      });
+    // Setup the options
+    var options = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      headers: req.headers,
+      path: join(parsedUrl.pathname, trailingSlash ? req.url : req.url.substring(1)),
+      method: req.method
+    };
 
-    if (port) request.set("x-forwarded-port", port);
+    // Enable forwarding headers
+    if(opts.xforward) {
+      // Get our forwarding info
+      var hostInfo = req.headers.host.split(":")
+        , resPath = req.originalUrl.replace(req.url, "");
 
-    // Pipe upstream and downstream
+      // We'll need to add a / if it's not on there
+      if(resPath.indexOf("/") !== 0) resPath = join("/", resPath);
+
+      // Pass along our headers
+      options.headers["x-forwarded-proto"] = req.connection.encrypted ? "https" : "http";
+      options.headers["x-forwarded-host"] = hostInfo[0];
+      options.headers["x-forwarded-path"] = resPath;
+
+      if (hostInfo[1]) options.headers["x-forwarded-port"] = hostInfo[1];
+    }
+
+    debug("sending proxy request", options);
+
+    // Make the request with the correct protocol
+    var request = protocols[parsedUrl.protocol.replace(":", "") || 'http'].request(options, function(response) {
+      debug("got response");
+
+      // Send down the statusCode and headers
+      debug("sending head", response.statusCode, response.headers);
+      res.writeHead(response.statusCode, response.headers);
+
+      // Pipe the response
+      debug("piping response");
+      response.pipe(res);
+    });
+
+    // Handle any timeouts that occur
+    request.setTimeout(opts.timeout || 10000, function() {
+      // Clean up the socket
+      // TODO is there a better way to do this? There's a 'socket hang up' error being emitted...
+      request.setSocketKeepAlive(false);
+      request.socket.destroy();
+
+      // Pass down the error
+      var err = new Error("Proxy to '"+endpoint+"' timed out");
+      request.emit("error", err);
+    });
+
+    // Pipe the client request upstream
     req.pipe(request);
-    request.pipe(res);
+
+    // Pass on our errors
+    request.on('error', next);
   }
 }
